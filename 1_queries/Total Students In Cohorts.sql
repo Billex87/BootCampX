67e9ef128355fc08da3a9704b9c5b105127d7361
Billy <billy_mckinnon@hotmail.com>
2021-02-02 03:53:12 +0000

    Added the Questions For Notes

--- 1_queries/Total Students In Cohorts.sql
+++ 1_queries/Total Students In Cohorts.sql
@@ -1,3 +1,12 @@
+/*Select the total number of students who were in the first 3 cohorts.
+
+Expected Result:
+
+ count 
+-------
+    48
+(1 row)*/
+
 SELECT count(id)
 FROM students 
 WHERE cohort_id IN (1,2,3);
